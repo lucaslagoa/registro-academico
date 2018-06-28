fb13ee48a95718920c7e19a434a503dfdbe57659
Bruno Rocha <bsrocha1005@gmail.com>
2018-06-28 00:03:54 -0300

    Fix: remoção e adequação dos métodos.

--- itemVenda.py
+++ itemVenda.py
@@ -17,10 +17,3 @@ class itemVenda(totalizavel.Totalizavel):
 	# imprime o itemVenda
 	def __str__(self): 
 		return self.produto + "\t" + self.quantidade + "\t" + str(self.valor) + "\t" + str(self.total())
-
-	# def total(self): FUNÇÃO DEPRECIADA, ISSO É COISA DE CALOURO, QUERO ORIENTAÇÃO A OBJETOS, CEEEEEERTO!
-	# 	total = 0;
-	# 	for x in range(0,len(self.venda.itens)):
-	# 		parcial = self.valor[x] * self.quantidade[x]
-	# 		print "\t" + self.itens[x] + "\t" + self.valor[x] + "\t" + parcial
-	# 		total += parcial
